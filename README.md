a3d384c85e4c111619ff98616e80b151873be592
Ernesto-Rubio <alex.chay9816@gmail.com>
2022-10-15 10:29:28 -0600

    se sube desarrollo para deportenis

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Ejercicio-2
+--para poder montar la bd de sqlite, es solamente colocar la carpeta bd en el disco local raiz
+por ejemplo(C:\db).
+
+
+-en la carperta del proyecto se encuentra el ejecutable de la bd sqlLite, carpeta (sqliteadmin)
---
+++ ejercicionumero2/src/ejercicionumero2/distribuidorBL/distribuidoresBL.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicionumero2.distribuidorBL;
+
+/**
+ *
+ * @author ernesto
+ */
+public class distribuidoresBL {
+    int ID;
+    String nombre,apellido_paterno, apellido_materno,calle,colonia,numero,fechaRegistro;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+    
+}
